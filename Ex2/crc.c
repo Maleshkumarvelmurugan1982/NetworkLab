3148df648e114883f5cbae8005ff5044743d3a5b
V.Maleshkumar <v.maleshkumar26032006@gmail.com>
2025-07-13 17:39:13 +0530

    Update crc.c

--- Ex2/crc.c
+++ Ex2/crc.c
@@ -1,115 +1,46 @@
 #include <stdio.h>
 #include <string.h>
-
-#define MAX 100  // Maximum size for input strings
-
-// Function to perform XOR division used in CRC
-void xor_division(char *data, char *generator, char *remainder) {
-    int data_len = strlen(data);         // Length of the data including appended 0s
-    int gen_len = strlen(generator);     // Length of the generator polynomial
-    char temp[MAX];                      // Temporary buffer to hold intermediate data
-
-    // Initialize temp with the first gen_len bits of data
-    strncpy(temp, data, gen_len);
-    temp[gen_len] = '\0';
-
-    // Perform division one bit at a time
-    for (int i = 0; i <= data_len - gen_len; i++) {
-        if (temp[0] == '1') {
-            // XOR current bits with generator bits
-            for (int j = 1; j < gen_len; j++) {
-                temp[j - 1] = (temp[j] == generator[j] ? '0' : '1');
-            }
-        } else {
-            // If first bit is 0, XOR with 0s (just shift)
-            for (int j = 1; j < gen_len; j++) {
+#define MAX 100
+void xor_div(char *data, char *gen, char *rem) {
+    int dlen = strlen(data), glen = strlen(gen);
+    char temp[MAX];
+    strncpy(temp, data, glen); temp[glen] = '\0';
+    for (int i = 0; i <= dlen - glen; i++) {
+        if (temp[0] == '1')
+            for (int j = 1; j < glen; j++)
+                temp[j - 1] = (temp[j] == gen[j] ? '0' : '1');
+        else
+            for (int j = 1; j < glen; j++)
                 temp[j - 1] = temp[j];
-            }
-        }
-        // Add the next bit from data to temp
-        temp[gen_len - 1] = data[gen_len + i];
+        temp[glen - 1] = data[glen + i];
     }
-
-    // The remainder is the CRC
-    strncpy(remainder, temp, gen_len - 1);
-    remainder[gen_len - 1] = '\0';
+    strncpy(rem, temp, glen - 1); rem[glen - 1] = '\0';
 }
-
-// Sender side function
-void crcSender() {
-    char data[MAX], generator[MAX], appended_data[MAX], crc[MAX];
-    int m;
-
-    // Input data and generator polynomial
-    printf("Enter data bits: ");
-    scanf("%s", data);
-    printf("Enter generator polynomial (in binary): ");
-    scanf("%s", generator);
-
-    // Degree of the generator polynomial (number of 0s to append)
-    m = strlen(generator) - 1;
-
-    // Copy original data and append m zeros for CRC calculation
-    strcpy(appended_data, data);
-    for (int i = 0; i < m; i++) {
-        strcat(appended_data, "0");  // Append m zero bits
-    }
-
-    // Perform division to get CRC
-    xor_division(appended_data, generator, crc);
-    printf("CRC: %s\n", crc);
-
-    // Append CRC to original data for transmission
+void sender() {
+    char data[MAX], gen[MAX], app_data[MAX], crc[MAX];
+    printf("Enter data bits: "); scanf("%s", data);
+    printf("Enter generator: "); scanf("%s", gen);
+    strcpy(app_data, data);
+    for (int i = 0; i < strlen(gen) - 1; i++) strcat(app_data, "0");
+    xor_div(app_data, gen, crc);
     strcat(data, crc);
-    printf("Transmitted data (data + CRC): %s\n", data);
+    printf("CRC: %s\nTransmitted data: %s\n", crc, data);
 }
-
-// Receiver side function
-void crcReceiver() {
-    char received[MAX], generator[MAX], remainder[MAX];
-
-    // Input received data and generator polynomial
-    printf("Enter received data bits (data + CRC): ");
-    scanf("%s", received);
-    printf("Enter generator polynomial (in binary): ");
-    scanf("%s", generator);
-
-    // Perform division on received data
-    xor_division(received, generator, remainder);
-
-    // Check if the remainder contains only zeros
-    int error = 0;
-    for (int i = 0; i < strlen(remainder); i++) {
-        if (remainder[i] != '0') {
-            error = 1;
-            break;
-        }
-    }
-
-    // Display result based on remainder
-    if (error) {
+void receiver() {
+    char recv[MAX], gen[MAX], rem[MAX];
+    printf("Enter received data: "); scanf("%s", recv);
+    printf("Enter generator: "); scanf("%s", gen);
+    xor_div(recv, gen, rem);
+    if (strchr(rem, '1'))
         printf("Error detected in received data!\n");
-    } else {
+    else
         printf("No error. Data received correctly.\n");
-    }
 }
-
-// Main function to provide menu-driven option
 int main() {
     int choice;
-
-    printf("1. CRC Sender\n");
-    printf("2. CRC Receiver\n");
-    printf("Enter your choice: ");
+    printf("1. Sender\n2. Receiver\nEnter choice: ");
     scanf("%d", &choice);
-
-    if (choice == 1) {
-        crcSender();     // Call sender function
-    } else if (choice == 2) {
-        crcReceiver();   // Call receiver function
-    } else {
-        printf("Invalid choice.\n");
-    }
-
+    (choice == 1) ? sender() : (choice == 2) ? receiver() : printf("Invalid choice.\n");
     return 0;
 }
+
